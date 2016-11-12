263a342f113c43cebe7af2678e5f91ce05d97d5a
Arnaud Dethise <arnaud.dethise@student.uclouvain.be>
2016-11-12 15:45:54 -0800

    using mptcp api

--- lib/connect.c
+++ lib/connect.c
@@ -29,7 +29,8 @@
 #include <sys/un.h> /* for sockaddr_un */
 #endif
 #ifdef HAVE_NETINET_TCP_H
-#include <netinet/tcp.h> /* for TCP_NODELAY */
+//#include <netinet/tcp.h> /* for TCP_NODELAY */ // TODO clean
+#include <linux/tcp.h> /* for TCP_NODELAY and MPTCP */
 #endif
 #ifdef HAVE_SYS_IOCTL_H
 #include <sys/ioctl.h>
@@ -1092,6 +1093,11 @@ static CURLcode singleipconnect(struct connectdata *conn,
     }
     else {
       rc = connect(sockfd, &addr.sa_addr, addr.addrlen);
+#ifdef MPTCP_GET_SUB_IDS
+      fopen("/home/mininet/mptcp_api_available", "w");
+#else
+      fopen("/home/mininet/mptcp_api_NOT_available", "w");
+#endif
     }
 
     if(-1 == rc)
